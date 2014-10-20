715f975b29f83983b5e29b5cf3857bd7c7359497
ahilan-thurairajah <ahilan@iname.com>
2014-10-20 07:30:53 -0400

    Initial version

---
+++ ContactController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DotNetNuke.Web.Api;
+using System.Collections.Generic ;
+using DotNetNuke.Entities.Users;
+using System.Linq;
+using DotNetNuke.Entities.Portals;
+
+namespace VignaharaServices
+{
+    public class ContactController : DnnApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public List<Contact> GetContacts(int pId)
+        {
+            int portalID = PortalController.Instance.GetCurrentPortalSettings().PortalId;
+            List<Contact> uiList = new List<Contact>() ;
+
+            foreach (UserInfo ui in UserController.GetUsers(portalID)) {
+                uiList.Add(new Contact() {  UserID=ui.UserID, 
+                                            DisplayName=ui.DisplayName,
+                                            Email = ui.Email,
+                                            UserName = ui.Username });
+            }
+
+            return uiList ;
+        }
+
+        public class Contact
+        {
+            public int UserID { get; set; }
+            public string DisplayName { get; set; }
+            public string Email { get; set; }
+            public string UserName { get; set; }
+
+        }
+    }
+}
---
+++ RouteMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DotNetNuke.Web.Api;
+
+namespace VignaharaServices
+{
+    public class RouteMapper : IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute("VignaharaServices", "default", "{controller}/{action}", new[] { "VignaharaServices" });
+        }
+    }
+}
